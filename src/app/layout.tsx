88b8c9e4d5806723703fdc79105bdac8c34f77f1
a.soltanian <ali76rez@gmail.com>
2025-06-26 15:27:05 +0330

    readme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,19 @@
 import Providers from "@/components/providers";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "../index.css";
 import { Suspense } from "react";
 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "urchat",
@@ -27,7 +27,9 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={` antialiased`}>
+      <body
+        className={`antialiased ${geistSans.variable} ${geistMono.variable}`}
+      >
         <Suspense fallback={null}>
           <Providers>
             <TooltipProvider>{children}</TooltipProvider>
--- src/components/code-block.tsx
+++ src/components/code-block.tsx
@@ -21,31 +21,36 @@ export function CodeBlock({
   const code = typeof children === "string" ? children.trim() : "";
   if (!inline) {
     return (
-      <div className="not-prose flex flex-col my-2 w-full">
-        <pre
-          {...props}
-          className={` w-full  dark:bg-zinc-900 p-4 pt-2 border border-zinc-200 dark:border-zinc-700 rounded-xl dark:text-zinc-50 text-zinc-900`}
-        >
-          <p className={`text-xs  py-2  `}>{language}</p>
-          <div className="sticky top-5">
-            <div className="absolute end-0 bottom-0 flex h-9 items-center pe-1">
-              <CopyButton text={typeof children === "string" ? children : ""} />
-            </div>
+      // <div className="not-prose flex flex-col my-2 w-[90%]">
+      <pre
+        className={`  dark:bg-zinc-900 p-4 pt-2 border border-zinc-200 dark:border-zinc-700 rounded-xl dark:text-zinc-50 text-zinc-900`}
+      >
+        <p className={`text-xs  py-2  `}>{language}</p>
+        <div className="sticky top-5">
+          <div className="absolute end-0 bottom-0 flex h-9 items-center pe-1">
+            <CopyButton text={typeof children === "string" ? children : ""} />
           </div>
-          <SyntaxHighlighter
-            language={language}
-            style={vscDarkPlus}
-            customStyle={{
-              padding: "0",
-              fontSize: "0.875rem",
-              background: "inherit",
-            }}
-            {...props}
-          >
-            {code}
-          </SyntaxHighlighter>
-        </pre>
-      </div>
+        </div>
+        <SyntaxHighlighter
+          language={language}
+          style={vscDarkPlus}
+          customStyle={{
+            padding: "0",
+            fontSize: "0.875rem",
+            background: "inherit",
+          }}
+          codeTagProps={{
+            style: {
+              whiteSpace: "pre-wrap",
+              wordBreak: "normal",
+              overflowWrap: "break-word",
+            },
+          }}
+        >
+          {code}
+        </SyntaxHighlighter>
+      </pre>
+      // </div>
     );
   } else {
     return (
